cffb8a9944c73f3badd27d851d71870b20e2c571
Abdelkarim Ouaha kabouri <hkb5006a@ensfsi.onmicrosoft.com>
2026-04-09 11:18:51 +0200

    Avancée TP 4, classe Humain

---
+++ src/histoire/HistoireTP4.java
@@ -0,0 +1,25 @@
+package histoire;
+
+import personnages.Humain;
+
+public class HistoireTP4 {
+
+	public static void main(String[] args) {
+
+		Humain prof = new Humain("Prof", "kombucha", 54);
+
+		prof.direBonjour();
+
+		System.out.println(
+				"(Prof) - J'ai " + prof.getArgent() + " sous en poche. Je vais pouvoir m'offrir une boisson à 12 sous");
+
+		prof.acheter("boisson", 12);
+		prof.boire();
+
+		System.out.println(
+				"(Prof) - J'ai " + prof.getArgent() + " sous en poche. Je vais pouvoir m'offrir un jeu à 2 sous");
+
+		prof.acheter("jeu", 2);
+		prof.acheter("kimono", 50);
+	}
+}
